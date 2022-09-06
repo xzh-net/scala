71a3d38af1fa8f99431681ac9498b63c96095326
xuzhihao <xuzhihao@163.com>
2022-09-06 22:54:47 +0800

    test

---
+++ README.md
@@ -0,0 +1,8 @@
+# spark3.1.3-scala2.12.11
+
+
+## 1. 应用列表
+
+```lua
+spark3-test			    --scala测试
+```
---
+++ spark3-test/src/main/scala/hello.scala
@@ -0,0 +1,6 @@
+
+object Hello {
+  def main(args: Array[String]): Unit = {
+    println("Scala Hello World")
+  }
+}
